d1d4f72d9c219daa795c30917f2986362ece72e1
ChickenRed181 <chicken181.red@gmail.com>
2024-10-01 13:02:45 +0600

    Add files via upload

---
+++ car.js
@@ -0,0 +1,25 @@
+const carName = 'Tesla Model S'
+let maxCarSpeed = 250
+let carOwner = 'Владелец: Иван'
+
+alert(carName)
+alert(maxCarSpeed + " км/ч")
+alert(carOwner)
+
+maxCarSpeed = 260
+console.log('У машины изменилась скорость:', maxCarSpeed)
+
+maxCarSpeed = 270
+console.log('У машины изменилась скорость:', maxCarSpeed)
+
+maxCarSpeed = 280
+console.log('У машины изменилась скорость:', maxCarSpeed)
+
+carOwner = 'Сергей'
+console.log('У машины изменился владелец:', carOwner)
+
+carOwner = 'Анна'
+console.log('У машины изменился владелец:', carOwner)
+
+carOwner = 'Дмитрий'
+console.log('У машины изменился владелец:', carOwner)
---
+++ index.js
@@ -0,0 +1,18 @@
+// JavaScript-код с созданием переменных
+
+/* 
+Автор:
+Семён Михалев
+*/
+
+const myName = "Семён"
+const mySurname = "Михалёв"
+const myFavoriteDrink = "Пиво"
+const myFavoriteAnimal = "Котопёс"
+const myFavoriteProgrammingLanguage = "JavaScript"
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
